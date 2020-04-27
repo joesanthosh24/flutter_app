bd0e1009d21b2e10de9c2fcee1731906eb030b62
Joe Santhosh <joe.santhosh88@yahoo.com>
2020-04-26 22:47:05 -0400

    Created front page layout of app with quiz and practice buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var materialApp = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Center(
-          child: Text('My App'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Center(
+            child: Text('Bible Quiz App'),
+          ),
+        ),
+        body: Container(
+          color: Colors.blueGrey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: Text(
+                      'Take Quiz',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  color: Colors.green,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: Text(
+                      'Practice with Cards',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-  runApp(materialApp);
+    );
+  }
 }
